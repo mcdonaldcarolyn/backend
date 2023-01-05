994af0cab0770506d36b1d66b3fb24541beddc55
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2023-01-05 13:38:00 -0500

    add paths to urls

--- settings.py
+++ settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'book',
 ]
 
 MIDDLEWARE = [
+    'corsheader.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'backend.urls'
 
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000",]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
